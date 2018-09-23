68aa1fb20ba75a191dacef33900216e776516e18
ElectronicGang <41317807+ElectronicGang@users.noreply.github.com>
2018-09-23 05:32:13 -0700

    Update BookingCTL.java
    class refactored

--- src/hotel/booking/BookingCTL.java
+++ src/hotel/booking/BookingCTL.java
@@ -14,16 +14,16 @@ import hotel.entities.RoomType;
 import hotel.utils.IOUtils;
 
 public class BookingCTL {
-	
-	
+
 	private static enum State {PHONE, ROOM, REGISTER, TIMES, CREDIT, APPROVED, CANCELLED, COMPLETED}	
 	
 	private BookingUI bookingUI;
 	private Hotel hotel;
-        private long confirmationNumber;
+
 	private Guest guest;
 	private Room room;
 	private double cost;
+        private long confirmationNumber;
 	
 	private State state;
 	private int phoneNumber;
@@ -32,22 +32,37 @@ public class BookingCTL {
 	private Date arrivalDate;
 	private int stayLength;
 
-	
 	public BookingCTL(Hotel hotel) {
 		this.bookingUI = new BookingUI(this);
 		this.hotel = hotel;
-		state = State.PHONE;
-		this.room = new Room(120,RoomType.DOUBLE);
+		state = State.CREDIT;
+                this.room = new Room(120,RoomType.DOUBLE);
                 this.guest = new Guest("Sundy", "Deerpark", 123456789);
 	}
 
-	
 	public void run() {		
 		IOUtils.trace("BookingCTL: run");
-		bookingUI.run();
+		getBookingUI().run();
 	}
-	
-	public BookingUI getBookingUI() {
+        
+        /**
+     * @return the confirmationNumber
+     */
+    public long getConfirmationNumber() {
+        return confirmationNumber;
+    }
+
+    /**
+     * @param confirmationNumber the confirmationNumber to set
+     */
+    public void setConfirmationNumber(long confirmationNumber) {
+        this.confirmationNumber = confirmationNumber;
+    }
+    
+    /**
+     * @return the bookingUI
+     */
+    public BookingUI getBookingUI() {
         return bookingUI;
     }
 
@@ -57,8 +72,11 @@ public class BookingCTL {
     public void setBookingUI(BookingUI bookingUI) {
         this.bookingUI = bookingUI;
     }
-	
-	 public State getState() {
+        
+         /**
+     * @return the state
+     */
+    public State getState() {
         return state;
     }
 
@@ -69,21 +87,10 @@ public class BookingCTL {
         this.state = state;
     }
 	
-	 public long getConfirmationNumber() {
-        return confirmationNumber;
-    }
-
-    /**
-     * @param confirmationNumber the confirmationNumber to set
-     */
-    public void setConfirmationNumber(long confirmationNumber) {
-        this.confirmationNumber = confirmationNumber;
-    }
-	
 	
 	public void phoneNumberEntered(int phoneNumber) {
-		if (state != State.PHONE) {
-			String mesg = String.format("BookingCTL: phoneNumberEntered : bad state : %s", state);
+		if (getState() != State.PHONE) {
+			String mesg = String.format("BookingCTL: phoneNumberEntered : bad state : %s", getState());
 			throw new RuntimeException(mesg);
 		}
 		this.phoneNumber = phoneNumber;
@@ -92,33 +99,31 @@ public class BookingCTL {
 		
 		if (isRegistered) {
 			guest = hotel.findGuestByPhoneNumber(phoneNumber);
-			bookingUI.displayGuestDetails(guest.getName(), guest.getAddress(), guest.getPhoneNumber());
-			this.state = State.ROOM;
-			bookingUI.setState(BookingUI.State.ROOM);
+			getBookingUI().displayGuestDetails(guest.getName(), guest.getAddress(), guest.getPhoneNumber());
+                        this.setState(State.ROOM);
+			getBookingUI().setState(BookingUI.State.ROOM);
 		}
 		else {
-			this.state = State.REGISTER;
-			bookingUI.setState(BookingUI.State.REGISTER);
+                    this.setState(State.REGISTER);
+			getBookingUI().setState(BookingUI.State.REGISTER);
 		}
 	}
 
-
 	public void guestDetailsEntered(String name, String address) {
-		if (state != State.REGISTER) {
-			String mesg = String.format("BookingCTL: guestDetailsEntered : bad state : %s", state);
+		if (getState() != State.REGISTER) {
+			String mesg = String.format("BookingCTL: guestDetailsEntered : bad state : %s", getState());
 			throw new RuntimeException(mesg);
 		}
 		guest = hotel.registerGuest(name, address, phoneNumber);
 		
-		bookingUI.displayGuestDetails(guest.getName(), guest.getAddress(), guest.getPhoneNumber());
-		state = State.ROOM;
-		bookingUI.setState(BookingUI.State.ROOM);
+		getBookingUI().displayGuestDetails(guest.getName(), guest.getAddress(), guest.getPhoneNumber());
+		setState(State.ROOM);
+		getBookingUI().setState(BookingUI.State.ROOM);
 	}
 
-
 	public void roomTypeAndOccupantsEntered(RoomType selectedRoomType, int occupantNumber) {
-		if (state != State.ROOM) {
-			String mesg = String.format("BookingCTL: roomTypeAndOccupantsEntered : bad state : %s", state);
+		if (getState() != State.ROOM) {
+			String mesg = String.format("BookingCTL: roomTypeAndOccupantsEntered : bad state : %s", getState());
 			throw new RuntimeException(mesg);
 		}
 		this.selectedRoomType = selectedRoomType;
@@ -128,18 +133,17 @@ public class BookingCTL {
 		
 		if (!suitable) {			
 			String notSuitableMessage = "\nRoom type unsuitable, please select another room type\n";
-			bookingUI.displayMessage(notSuitableMessage);
+			getBookingUI().displayMessage(notSuitableMessage);
 		}
 		else {
-			state = State.TIMES;
-			bookingUI.setState(BookingUI.State.TIMES);
+			setState(State.TIMES);
+			getBookingUI().setState(BookingUI.State.TIMES);
 		}
 	}
 
-
 	public void bookingTimesEntered(Date arrivalDate, int stayLength) {
-		if (state != State.TIMES) {
-			String mesg = String.format("BookingCTL: bookingTimesEntered : bad state : %s", state);
+		if (getState() != State.TIMES) {
+			String mesg = String.format("BookingCTL: bookingTimesEntered : bad state : %s", getState());
 			throw new RuntimeException(mesg);
 		}
 		this.arrivalDate = arrivalDate;
@@ -159,20 +163,19 @@ public class BookingCTL {
 					format.format(arrivalDate),
 					format.format(departureDate));
 			
-			bookingUI.displayMessage(notAvailableStr);
+			getBookingUI().displayMessage(notAvailableStr);
 		}
 		else {
 			cost = selectedRoomType.calculateCost(arrivalDate, stayLength);
 			String description = selectedRoomType.getDescription();
-			bookingUI.displayBookingDetails(description, arrivalDate, stayLength, cost);
-			state = State.CREDIT;
-			bookingUI.setState(BookingUI.State.CREDIT);
+			getBookingUI().displayBookingDetails(description, arrivalDate, stayLength, cost);
+			setState(State.CREDIT);
+			getBookingUI().setState(BookingUI.State.CREDIT);
 		}
 	}
 
-
 	public void creditDetailsEntered(CreditCardType type, int number, int ccv) {
-		   //checking the state whether it is credit
+            //checking the state whether it is credit
            if (getState() != State.CREDIT) {
       String message = String.format("BookingCTL: BookingTimesEntered : bad state : %s", new Object[] { getState()});
       throw new RuntimeException(message);
@@ -209,22 +212,18 @@ public class BookingCTL {
             setState(State.COMPLETED);
             getBookingUI().setState(BookingUI.State.COMPLETED);
          }
-	}
-
+   }
 
 	public void cancel() {
 		IOUtils.trace("BookingCTL: cancel");
-		bookingUI.displayMessage("Booking cancelled");
-		state = State.CANCELLED;
-		bookingUI.setState(BookingUI.State.CANCELLED);
+		getBookingUI().displayMessage("Booking cancelled");
+		setState(State.CANCELLED);
+		getBookingUI().setState(BookingUI.State.CANCELLED);
 	}
-	
 	
 	public void completed() {
 		IOUtils.trace("BookingCTL: completed");
-		bookingUI.displayMessage("Booking completed");
+		getBookingUI().displayMessage("Booking completed");
 	}
-
-	
 
 }
