c9e522d454afe5e7d0248ebcff6635758c6bbf2c
Sandeep Hettiarachchi <sandeeprahan@gmail.com>
2018-09-23 19:54:37 -0700

    Code complete in recordServeiceCTL

--- src/hotel/service/RecordServiceCTL.java
+++ src/hotel/service/RecordServiceCTL.java
@@ -6,36 +6,34 @@ import hotel.entities.ServiceType;
 import hotel.utils.IOUtils;
 
 public class RecordServiceCTL {
-	
-	private static enum State {ROOM, SERVICE, CHARGE, CANCELLED, COMPLETED};
-	
+	/* Class global variables declaration */
+	private static enum State {ROOM, SERVICE, CHARGE, CANCELLED, COMPLETED};	
 	private Hotel hotel;
 	private RecordServiceUI recordServiceUI;
-	private State state;
-	
+	private State state;	
 	private Booking booking;
 	private int roomNumber;
 
-
+        /* Booking overloaded constructor  */
 	public RecordServiceCTL(Hotel hotel) {
 		this.recordServiceUI = new RecordServiceUI(this);
 		state = State.ROOM;
 		this.hotel = hotel;
 	}
 
-	
+	/* Method to run the class */
 	public void run() {		
 		IOUtils.trace("PayForServiceCTL: run");
 		recordServiceUI.run();
 	}
 
-
+        /* Method to enter room number */
 	public void roomNumberEntered(int roomNumber) {
-		if (state != State.ROOM) {
+		if (state != State.ROOM) { //Chack the state and throw error message if state is not ROOM
 			String mesg = String.format("PayForServiceCTL: roomNumberEntered : bad state : %s", state);
 			throw new RuntimeException(mesg);
 		}
-		booking = hotel.findActiveBookingByRoomId(roomNumber);
+		booking = hotel.findActiveBookingByRoomId(roomNumber); // Find the booking by room id
 		if (booking == null) {
 			String mesg = String.format("No active booking for room id: %d", roomNumber);
 			recordServiceUI.displayMessage(mesg);
@@ -47,24 +45,29 @@ public class RecordServiceCTL {
 		}
 	}
 	
-	
+	/* Method to genter service details */
 	public void serviceDetailsEntered(ServiceType serviceType, double cost) {
-		// TODO Auto-generated method stub
+            if (state != State.SERVICE) { //Chack the state and throw error message if state is not ROOM
+                String mesg = String.format("PayForServiceCTL: serviceDetailsEntered : bad state : %s", new Object[] { state });
+                throw new RuntimeException(mesg);
+            }
+            hotel.addServiceCharge(roomNumber, serviceType, cost);    // Add service charges
+            recordServiceUI.displayServiceChargeMessage(roomNumber, cost, serviceType.getDescription());
+            state = State.COMPLETED;
+            recordServiceUI.setState(RecordServiceUI.State.COMPLETED); // set the state as complete
 	}
 
-
+        /* Method to display cancel message */
 	public void cancel() {
 		recordServiceUI.displayMessage("Pay for service cancelled");
 		state = State.CANCELLED;
-		recordServiceUI.setState(RecordServiceUI.State.CANCELLED);
+		recordServiceUI.setState(RecordServiceUI.State.CANCELLED); //set the state as cancelled
 	}
 
-
+        /* Method to display completer message */
 	public void completed() {
 		recordServiceUI.displayMessage("Pay for service completed");
 	}
 
-
-	
 
 }
